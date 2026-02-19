0e0838e8faeee07b51efcfafe5eec5a9289d3394
Oleksandr <zombozo223@gmail.com>
2026-02-19 11:34:27 +0200

    update

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,6 @@
-function makeTransaction(quntity, pricePerDroid) {
-  const totalPrice = quntity * pricePerDroid;
-  return `You ordered ${quntity} droids worth ${totalPrice} credits!`;
+function makeTransaction(quantity, pricePerDroid) {
+  const totalPrice = quantity * pricePerDroid;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
